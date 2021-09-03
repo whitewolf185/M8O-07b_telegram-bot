bb45c921172b555e16927bb637c95a733e574992
whitewolf185 <whitewolf.mot185@gmail.com>
2021-09-03 22:49:53 +0300

    initial commit. It must work

---
+++ index.js
@@ -0,0 +1,163 @@
+const fs = require('fs');
+const TgBot = require('node-telegram-bot-api');
+const readLine = require('readline-sync')
+let token = '';
+
+let options = {};
+
+function write_to_log(text){
+    let date = new Date;
+    let data = '[ ' + date + ' ]\n' + text + '\n\n';
+    fs.writeFileSync(__dirname + '/logfile.log', data, {
+        flag: 'a+'
+    });
+}
+
+function add_trusted_users(question){
+    options.trustedUsers.push(question.who);
+    fs.writeFileSync(__dirname + '/options.txt', JSON.stringify(options));
+}
+
+function create_queue(queueListening){
+    let str = '';
+    for (let i = 0; i < queueListening.queue.length; i++) {
+        str += (i+1) + '. ' + queueListening.queue[i] + '\n';
+    }
+
+    return str;
+}
+
+let start = new Promise( ((resolve, reject) => {
+    try {
+        token = fs.readFileSync(__dirname + '/token.txt', "utf-8");
+    }
+    catch (e) {
+        if (e.code === 'ENOENT'){
+            token = readLine.question("Enter token...\n",{});
+            fs.writeFileSync(__dirname + '/token.txt',token);
+            resolve(token)
+        }
+        else{
+            reject(e);
+        }
+    }
+
+    try {
+        let str_options = fs.readFileSync(__dirname + '/options.txt', "utf-8");
+        options = JSON.parse(str_options);
+    }
+    catch (e){
+        if(e.code === 'ENOENT'){
+            console.log('options empty');
+            write_to_log('options empty');
+            options = {
+                trustedUsers: []
+            };
+        }
+    }
+
+
+    resolve(token);
+}))
+
+start.then(
+    token => {
+        const bot = new TgBot(token, {
+            polling: true
+        })
+
+        const commands = ["/newqueue", "/donequeue"];
+        let question = {
+            bool: false,
+            who: 0,
+            username: '',
+            chat: 0
+        };
+
+        let queueListening = {
+            bool: false,
+            first: true,
+            queue: [],
+            str: '',
+            count: 0
+        };
+
+        bot.onText(/\/newqueue/, (msg) => {
+            if (options.trustedUsers.indexOf(msg.from.id) === -1){
+                bot.sendMessage(msg.chat.id, "Ты не достоин");
+            }
+            else{
+                console.log("im here");
+                bot.sendMessage(msg.chat.id, "Начинаю очередь...\nПишите _Я_, если хотите добавиться в очердь.",{
+                    parse_mode: "Markdown"
+                });
+                queueListening.bool = true;
+            }
+        })
+
+        bot.onText(/\/donequeue/, (msg) => {
+            if(queueListening.bool){
+                queueListening.str = create_queue(queueListening);
+                bot.sendMessage(msg.chat.id, "*Вот такая наша очередь:*\n" + queueListening.str ,{
+                    parse_mode: "Markdown"
+                });
+            }
+            queueListening.bool = false;
+        })
+
+        bot.onText(/\/showqueue/, (msg) => {
+            if(queueListening.bool){
+                bot.sendMessage(msg.chat.id, "*Сейчас очередь такая:*\n" + queueListening.str ,{
+                    parse_mode: "Markdown"
+                });
+            }
+        })
+
+        bot.onText(/07b_bot я достоин/, (msg) => {
+            console.log('requested');
+            bot.sendMessage(475513670, msg.from.username + " достоин?");
+            question.bool = true;
+            question.who = msg.from.id;
+            question.username = msg.from.username;
+            question.chat = msg.chat.id;
+            write_to_log(msg.from.username + ' | id:' + msg.from.id + ' requested access to trusted Users');
+        })
+
+
+        bot.on('message', (msg) => {
+            if(question.bool){
+                if(msg.from.id === 475513670){
+                    if(msg.text.toLowerCase() === '07b_bot да') {
+                        add_trusted_users(question);
+                        bot.sendMessage(question.chat, "Добро пожаловать " + question.username);
+                        question.bool = false;
+                    }
+
+                    else{
+                        bot.sendMessage(question.chat, "Мне сказали ты не достоин");
+                        question.bool = false;
+                    }
+                }
+            }
+
+            else if(queueListening){
+                if(msg.text.toLowerCase() === 'я' && queueListening.queue.indexOf(msg.from.username) === -1){
+                    console.log('added');
+                    queueListening.count++;
+                    queueListening.queue.push(msg.from.username);
+                    bot.sendMessage(msg.chat.id, msg.from.username + ' добавлен ' + queueListening.count + '-м');
+                }
+            }
+
+            console.log(msg);
+        })
+
+
+
+    }
+)
+
+    //something goes wrong
+    .catch(e => {
+        write_to_log('Something wrong with token');
+    })
